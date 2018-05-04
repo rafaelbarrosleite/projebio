cccd0c22277a2be5fc38e14a5cfd5a35d415bc91
oliveirabah <barbarasilvapp@gmail.com>
2018-05-04 09:31:21 -0300

    corrigindo caminho da pasta e organizando css

--- application/views/projebio.php
+++ application/views/projebio.php
@@ -7,31 +7,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<title>Projebio</title>
 		<link rel="shortcut icon" href="favicon.ico" />
 
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>content/estilos/estilo.css" /> 
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>content/estilos/estilos.css" /> 
 	</head>
 	<body>
 		<header class="cabecalho">
 			<div class="conteudo">
-				<div class="blocos">
-					<a href="" class="logo">
-						<img src="<?php echo base_url();?>content/Imagens/logo.png" alt="Imagem da empresa">
+				<div class="grade">
+					<a href="" class="logo grade__coluna_2">
+						<img src="<?php echo base_url();?>content/img/logo.png" alt="Logo da empresa">
 					</a> 
-			<!-- 		<a href="" class="logo">
-						<img src="http://via.placeholder.com/185x63" alt="Imagem da empresa">
-					</a> -->
-					<nav class="links"> 
-						<a href="">Página inicial</a>
-						<a href="">Produtos</a>
-						<a href="">Indústria</a>
-						<a href="">Quem somos</a>
-						<a href="">Informações</a>
-						<a href="">Fale Conosco</a>
+					<nav class="menu grade__coluna_7"> 
+						<a class="menu_link" href="">Página inicial</a>
+						<a class="menu_link" href="">Produtos</a>
+						<a class="menu_link" href="">Indústria</a>
+						<a class="menu_link" href="">Quem somos</a>
+						<a class="menu_link" href="">Fale Conosco</a>
 					</nav>
-					<section class="contato-cabecalho">
+					<section class="contato-cabecalho grade__coluna_3 grade">
+						<img class="icone" src="<?php echo base_url();?>content/img/ic-phone.png">
+						<div class="texto"> 
 							Entre em contato
 							<div class="numero-do-telefone">
 								(67) 9 9956-8208
 							</div>
+						</div>
 					</section>
 				</div>
 			</div>	
@@ -39,31 +38,31 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 		<section> 
 			<div class="banner">
-				<img src="http://via.placeholder.com/1366x350" alt="">
+				<img src="<?php echo base_url();?>content/img/imagem-banner" alt="">
 			</div>
 		</section>
 		<main class="conteudo">
 			<section class="produtos"> 
 				<h2 class="titulo">Conheça nossos produtos</h2>
-				<p class="descricao-do-bloco">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, ex.</p>
-				<div class="blocos lista-de-produtos">
-					<div class="lista-de-produtos_item">
-						<img src="http://via.placeholder.com/450x280" alt="">
-						<h3>Nome do produto</h3>
+				<p class="descricao-do-bloco">Trabalhamos com processamento da mamona onde são obtidos dois produtos: o óleo de mamona e a torta de mamona.</p>
+				<div class="grade lista-de-produtos">
+					<div class="grade__coluna_6 lista-de-produtos_item">
+						<img src="<?php echo base_url();?>content/img/oleo-de-mamona.jpg" alt="">
+						<h3>Óleo de mamona</h3>
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, adipisci.</p>
 					</div>
-					<div class="lista-de-produtos_item">
-						<img src="http://via.placeholder.com/450x280" alt="">
-						<h3>Nome do produto</h3>
+					<div class="grade__coluna_6 lista-de-produtos_item">
+						<img src="<?php echo base_url();?>content/img/adubos-organico.jpg" alt="">
+						<h3>Torta de mamona</h3>
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, adipisci.</p>
 					</div>
 				</div>
 			</section>
 		</main>
-		<div class="sobre-a-empresa">
+		<div class="sobre-a-empresa" style="background: url('<?php echo base_url();?>content/img/imagem2.jpg");		">
 			<main class="conteudo">
-				<div class="blocos">
-					<img class="imagem-da-empresa" src="http://via.placeholder.com/300x180" alt="">
+				<div class="grade">
+					<img class="imagem-da-empresa" src="<?php echo base_url();?>content/img/edificio.jpg" alt="">
 					<div class="informacoes-sobre-a-empresa"> 
 						<h2>A PROJEBIO</h2>
 						<p>
@@ -80,68 +79,88 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			</main>
 		</div>
 		<section class="conteudo fotos-da-empresa">
-			<div class="blocos">
-				<div class="fotos-da-empresa_item">
-					<img src="http://via.placeholder.com/200x180" alt="">
+			<div class="grade">
+				<div class="fotos-da-empresa_item grade__coluna_3">
+					<img class="imagem-da-empresa" src="<?php echo base_url();?>content/img/edificio.jpg" alt="">
 					<h3>Lorem Ipsum Dolor</h3>
 				</div>
-				<div class="fotos-da-empresa_item">
-					<img src="http://via.placeholder.com/200x180" alt="">
+				<div class="fotos-da-empresa_item grade__coluna_3">
+					<img class="imagem-da-empresa" src="<?php echo base_url();?>content/img/edificio.jpg" alt="">
 					<h3>Lorem Ipsum Dolor</h3>
 				</div>
-				<div class="fotos-da-empresa_item">
-					<img src="http://via.placeholder.com/200x180" alt="">
+				<div class="fotos-da-empresa_item grade__coluna_3">
+					<img class="imagem-da-empresa" src="<?php echo base_url();?>content/img/edificio.jpg" alt="">
 					<h3>Lorem Ipsum Dolor</h3>
 				</div>
 			</div>
 			
 		</section>
-		<section class="conteudo entre-em-contato">
-			<h2 class="titulo">Entre em contato conosco</h2>
-			<div class="blocos">
-				<form class="formulario-de-contato">
-					<div class="formulario-de-contato_item">
-						<label for="nome">Qual o seu nome?</label>
-						<input type="text" id="nome">
-					</div> 
-					<div class="formulario-de-contato_item">
-						<label for="telefone">Telefone para contato</label>
-						<input type="text" id="telefone">
-					</div> 
-					<div class="formulario-de-contato_item">
-						<label for="email">E-mail:</label>
-						<input type="text" id="email">
-					</div> 
-					<div class="formulario-de-contato_item">
-						<label for="comentario">Diga nos o que precisa:</label>
-						<textarea name="" id="comentario" cols="30" rows="10"></textarea>
-					</div> 
-					<button type="submit">Enviar</button>
-				</form>
-				<div class="localizacao">
+		<section class="entre-em-contato">
+			<div class="conteudo">
+				<h2 class="titulo">Venha nos conhecer</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci dolor, iste facere!</p>
+			</div>
+			<div class="localizacao grade">
+				<div class="localizacao_item grade__coluna_5">
 					<div class="mapa">
-						<img src="http://via.placeholder.com/200x180" alt="">
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3737.9834709004294!2d-54.61935268555817!3d-20.465872060479814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9486e618848ea35d%3A0x865d3df35f71a980!2sR.+Quinze+de+Novembro%2C+310+-+Centro%2C+Campo+Grande+-+MS%2C+79002-140!5e0!3m2!1spt-BR!2sbr!4v1525314798460" width="500" height="282" frameborder="0" style="border:0" allowfullscreen></iframe>
 					</div>
 					<div class="texto-endereco">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, ipsum unde, reprehenderit, expedita modi iusto est tempore aliquid officiis eveniet maxime velit impedit. Quis praesentium, excepturi illo nobis, aperiam dolores!
+						<strong>Escritório:</strong> Rua XV de Novembro, 310, Sala 901 - Campo Grande/MS - Cep: 79.002-040 
+					</div>
+					<div class="telefone">
+						lorem1
+					</div>
+				</div>
+				<div class="localizacao_item grade__coluna_5">
+					<div class="mapa">
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d142542.43137818173!2d-54.459035340562366!3d-20.145231166058277!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94fa68e43b9afbfd%3A0x609247e722644dd7!2sBR-163%2C+Mato+Grosso+do+Sul!5e0!3m2!1spt-BR!2sbr!4v1525314764992" width="500" height="282" frameborder="0" style="border:0" allowfullscreen></iframe>
+					</div>
+					<div class="texto-endereco">
+						<strong>Fábrica:</strong> 
+						Rod BR 163 S/N KM 461 - Jaraguari/MS - Cep: 794.40-000
 					</div>
 					<div class="telefone">
 						lorem1
 					</div>
 				</div>
 			</div>
+			<div class="conteudo">
+				<h2 class="titulo">ou se preferir, entre em contato conosco</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci dolor, iste facere!</p>
+				<form class="formulario-de-contato">
+					<div class="grade">
+						<div class="formulario-de-contato_item grade__coluna_5">
+							<label for="nome">Qual o seu nome?</label>
+							<input type="text" id="nome">
+						</div> 
+						<div class="formulario-de-contato_item  grade__coluna_2">
+							<label for="telefone">Telefone:</label>
+							<input type="text" id="telefone">
+						</div> 
+						<div class="formulario-de-contato_item  grade__coluna_4">
+							<label for="email">E-mail:</label>
+							<input type="text" id="email">
+						</div>
+					</div>
+					<div class="grade">
+						<div class="formulario-de-contato_item  grade__coluna_12">
+							<label for="comentario">Diga nos o que precisa:</label>
+							<textarea name="" id="comentario" cols="30" rows="10"></textarea>
+						</div> 
+					</div>
+					<button type="submit">Enviar</button>
+				</form>
+			</div>
+
 		</section>
 	</body>
 </html>
 
 <style type="text/css">
 
-*{margin:0; padding:0; border:0; list-style:none;}
 
 
-body{
-	font-family: 'Roboto';
-}
 
 h2{
 	font-size: 24px;
@@ -156,9 +175,7 @@ h2{
 	line-height: 22px;
 }
 
-.body{
-	
-}
+
 
 .blocos{
 	display: flex;
@@ -167,73 +184,6 @@ h2{
 
 
 
-.lista-de-produtos .lista-de-produtos_item:first-child{
-	    margin-right: 10%;
-}
-
-.lista-de-produtos .lista-de-produtos_item h3{
-
-}
-
-.cabecalho{
-	display: flex;
-	height: 70px;
-	padding: 12px;
-	align-items: center;
-
-
-}
-	.logo{
-		width: 200px;
-	}
-	.paginas{
-
-	}
-
-	.contato-cabecalho{
-
-	}
-	.numero-do-telefone{
-		display: block;
-		font-weight: bold;
-		display: block;
-	}
-
-
-	.conteudo{
-		width: 1020px;
-		max-width: 100%;
-		margin: 0 auto;
-		display: block;
-	}
-
-.informacoes-sobre-a-empresa{
-	width: 500px;
-}
-
-
-.sobre-a-empresa{
-	background: url(http://via.placeholder.com/300x180);
-    height: 357px;
-    margin-top: 60px;
-    padding: 70px 0 0 0;
-    background-size: 100%;
-    margin-bottom: 45px;
-    background-attachment: fixed;
-    background-position: center;
-}
-
-.imagem-da-empresa{
-	margin-right: 20px;
-}
-
-.informacoes-sobre-a-empresa{
-
-}
-
-.fotos-da-empresa_item{
-	margin: 15px;
-}
 
 
 
@@ -241,49 +191,6 @@ h2{
 
 
 
-/*sobre a projebio*/
 
 
-/*contato*/
-
-.formulario-de-contato{
-	width: 40%;
-	margin-right: 10%;
-}
-.formulario-de-contato_item{
-	margin: 10px 0;
-
-}
-
-label{
-	font-size: 14px;
-	font-weight: bold;
-	margin-bottom: 8px;
-	display: block;
-}
-
-input{
-	height: 44px;
-	box-sizing: border-box;
-}
-
-button{
-	background-color: green;
-	padding: 8px;
-	height: 44px;
-	box-sizing: border-box;
-	border-radius: 3px;
-	width: 200px;
-	color: white;
-	border: none;
-
-}
-
-
-input, textarea{
-	border: 1px solid #cecece;
-	border-radius: 3px;
-	padding: 12px;
-
-}
 </style>
